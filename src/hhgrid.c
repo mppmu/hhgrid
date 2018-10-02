4841cf3baf4633e80b7e0e199f10aeb309d6234a
Stephen Jones <sjones@mpp.mpg.de>
2018-10-02 15:32:03 +0200

    bugfix when obtaining paths for files in current directory

--- src/hhgrid.c
+++ src/hhgrid.c
@@ -46,12 +46,15 @@ PyObject* grid_initialize(const char* grid_name)
     char* grid_file_path;
     size_t len_path_sep = strlen(path_sep);
     size_t len_grid_name = strlen(grid_name);
+
     char* events_file_path;
     char* events_name = "events.cdf";
     size_t len_events_name = strlen(events_name);
+
     char* creategrid_file_path;
     char* creategrid_name = "creategrid.py";
     size_t len_creategrid_name = strlen(creategrid_name);
+
     char* pythonpath = strdup(getenv("PYTHONPATH"));
     char* result = strtok( pythonpath, delims );
     size_t len_result = 0;
@@ -60,7 +63,14 @@ PyObject* grid_initialize(const char* grid_name)
     if( access(grid_name, F_OK) != -1 && access(grid_name, R_OK) != -1 )
     {
         grid_file_path = (char*) malloc(len_grid_name + 1); // +1 for null terminator
-        memcpy(grid_file_path, grid_name, len_grid_name);
+        memcpy(grid_file_path, grid_name, len_grid_name + 1);
+
+        events_file_path = (char*) malloc (len_events_name + 1); // +1 for null terminator
+        memcpy(events_file_path, events_name, len_events_name + 1);
+
+        creategrid_file_path = (char*) malloc (len_creategrid_name + 1); // +1 for null terminator
+        memcpy(creategrid_file_path, creategrid_name, len_creategrid_name + 1);
+
         search_paths = 0;
         setenv("PYTHONPATH", ".", 1); // Set PYTHONPATH to look here
     }
@@ -69,15 +79,48 @@ PyObject* grid_initialize(const char* grid_name)
     while( search_paths == 1 && result != NULL )
     {
         len_result = strlen(result);
+
         grid_file_path = (char*) malloc (len_result + len_path_sep + len_grid_name + 1); // +1 for null terminator
         memcpy(grid_file_path, result, len_result);
         memcpy(grid_file_path + len_result, path_sep, len_path_sep);
         memcpy(grid_file_path + len_result + len_path_sep, grid_name, len_grid_name + 1); // +1 for null terminator
+
         printf("Searching for %s in: %s ", grid_name, grid_file_path);
         if( access(grid_file_path, F_OK) != -1 && access(grid_file_path, R_OK) != -1 )
         {
             printf("found\n");
             search_paths = 0;
+
+            // Now check for other required files
+            // events.cdf
+            events_file_path = (char*) malloc (len_result + len_path_sep + len_events_name + 1); // +1 for null terminator
+            memcpy(events_file_path, result, len_result);
+            memcpy(events_file_path + len_result, path_sep, len_path_sep);
+            memcpy(events_file_path + len_result + len_path_sep, events_name, len_events_name + 1); // +1 for null terminator
+            printf("Searching for %s in: %s ", events_name, events_file_path);
+            if( access(events_file_path, F_OK) != -1 && access(events_file_path, R_OK) != -1 )
+            {
+                printf("found\n");
+            } else {
+                printf("not found\n");
+                printf("ERROR: Failed to find events.cdf");
+                exit(1);
+            }
+
+            // creategrid.py
+            creategrid_file_path = (char*) malloc (len_result + len_path_sep + len_creategrid_name + 1); // +1 for null terminator
+            memcpy(creategrid_file_path, result, len_result);
+            memcpy(creategrid_file_path + len_result, path_sep, len_path_sep);
+            memcpy(creategrid_file_path + len_result + len_path_sep, creategrid_name, len_creategrid_name + 1); // +1 for null terminator
+            printf("Searching for %s in: %s ", creategrid_name, creategrid_file_path);
+            if( access(creategrid_file_path, F_OK) != -1 && access(creategrid_file_path, R_OK) != -1 )
+            {
+                printf("found\n");
+            } else {
+                printf("not found\n");
+                printf("ERROR: Failed to find creategrid.py");
+                exit(1);
+            }
         } else
         {
             printf("not found\n");
@@ -88,37 +131,6 @@ PyObject* grid_initialize(const char* grid_name)
                 exit(1);
             }
         }
-    }
-
-    // Now check for other required files
-    // events.cdf
-    events_file_path = (char*) malloc (len_result + len_path_sep + len_events_name + 1); // +1 for null terminator
-    memcpy(events_file_path, result, len_result);
-    memcpy(events_file_path + len_result, path_sep, len_path_sep);
-    memcpy(events_file_path + len_result + len_path_sep, events_name, len_events_name + 1); // +1 for null terminator
-    printf("Searching for %s in: %s ", events_name, events_file_path);
-    if( access(events_file_path, F_OK) != -1 && access(events_file_path, R_OK) != -1 )
-    {
-        printf("found\n");
-    } else {
-        printf("not found\n");
-        printf("ERROR: Failed to find events.cdf");
-        exit(1);
-    }
-
-    // creategrid.py
-    creategrid_file_path = (char*) malloc (len_result + len_path_sep + len_creategrid_name + 1); // +1 for null terminator
-    memcpy(creategrid_file_path, result, len_result);
-    memcpy(creategrid_file_path + len_result, path_sep, len_path_sep);
-    memcpy(creategrid_file_path + len_result + len_path_sep, creategrid_name, len_creategrid_name + 1); // +1 for null terminator
-    printf("Searching for %s in: %s ", creategrid_name, creategrid_file_path);
-    if( access(creategrid_file_path, F_OK) != -1 && access(creategrid_file_path, R_OK) != -1 )
-    {
-        printf("found\n");
-    } else {
-        printf("not found\n");
-        printf("ERROR: Failed to find creategrid.py");
-        exit(1);
     }
 
     printf("PYTHONPATH: %s\n", getenv("PYTHONPATH"));
@@ -169,6 +181,8 @@ PyObject* grid_initialize(const char* grid_name)
     // Cleanup
     free(pythonpath);
     free(grid_file_path);
+    free(events_file_path);
+    free(creategrid_file_path);
     Py_DECREF(pModule);
     Py_DECREF(pClass);
     Py_DECREF(pGridName);

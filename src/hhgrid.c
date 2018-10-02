87fef89cb6c2bbd5a69911b0b0954209bfd1e669
Stephen Jones <sjones@mpp.mpg.de>
2018-10-02 12:31:44 +0200

    provide more verbose errors

--- src/hhgrid.c
+++ src/hhgrid.c
@@ -41,13 +41,20 @@ void python_printinfo()
 PyObject* grid_initialize(const char* grid_name)
 {
     int search_paths = 1;
-    char* grid_file_path;
     char* delims = ":";
     char* path_sep = "/";
+    char* grid_file_path;
     size_t len_path_sep = strlen(path_sep);
     size_t len_grid_name = strlen(grid_name);
+    char* events_file_path;
+    char* events_name = "events.cdf";
+    size_t len_events_name = strlen(events_name);
+    char* creategrid_file_path;
+    char* creategrid_name = "creategrid.py";
+    size_t len_creategrid_name = strlen(creategrid_name);
     char* pythonpath = strdup(getenv("PYTHONPATH"));
     char* result = strtok( pythonpath, delims );
+    size_t len_result = 0;
 
     // Check if grid_name is accessible as-is
     if( access(grid_name, F_OK) != -1 && access(grid_name, R_OK) != -1 )
@@ -61,7 +68,7 @@ PyObject* grid_initialize(const char* grid_name)
     // Else search PYTHONPATH for grid_name
     while( search_paths == 1 && result != NULL )
     {
-        size_t len_result = strlen(result);
+        len_result = strlen(result);
         grid_file_path = (char*) malloc (len_result + len_path_sep + len_grid_name + 1); // +1 for null terminator
         memcpy(grid_file_path, result, len_result);
         memcpy(grid_file_path + len_result, path_sep, len_path_sep);
@@ -83,22 +90,80 @@ PyObject* grid_initialize(const char* grid_name)
         }
     }
 
+    // Now check for other required files
+    // events.cdf
+    events_file_path = (char*) malloc (len_result + len_path_sep + len_events_name + 1); // +1 for null terminator
+    memcpy(events_file_path, result, len_result);
+    memcpy(events_file_path + len_result, path_sep, len_path_sep);
+    memcpy(events_file_path + len_result + len_path_sep, events_name, len_events_name + 1); // +1 for null terminator
+    printf("Searching for %s in: %s ", events_name, events_file_path);
+    if( access(events_file_path, F_OK) != -1 && access(events_file_path, R_OK) != -1 )
+    {
+        printf("found\n");
+    } else {
+        printf("not found\n");
+        printf("ERROR: Failed to find events.cdf");
+        exit(1);
+    }
+
+    // creategrid.py
+    creategrid_file_path = (char*) malloc (len_result + len_path_sep + len_creategrid_name + 1); // +1 for null terminator
+    memcpy(creategrid_file_path, result, len_result);
+    memcpy(creategrid_file_path + len_result, path_sep, len_path_sep);
+    memcpy(creategrid_file_path + len_result + len_path_sep, creategrid_name, len_creategrid_name + 1); // +1 for null terminator
+    printf("Searching for %s in: %s ", creategrid_name, creategrid_file_path);
+    if( access(creategrid_file_path, F_OK) != -1 && access(creategrid_file_path, R_OK) != -1 )
+    {
+        printf("found\n");
+    } else {
+        printf("not found\n");
+        printf("ERROR: Failed to find creategrid.py");
+        exit(1);
+    }
+
     printf("PYTHONPATH: %s\n", getenv("PYTHONPATH"));
     printf("Grid Path: %s\n", grid_file_path);
+    printf("Events file Path: %s\n", events_file_path);
+    printf("CreateGrid file Path: %s\n", creategrid_file_path);
 
     PyObject* pModule = PyImport_ImportModule("creategrid");
+    if(pModule == NULL)
+    {
+        PyErr_Print();
+        printf("Failed to load creategrid.py: please check that you have numpy and scipy installed");
+    }
     assert(pModule != NULL);
 
     PyObject* pClass = PyObject_GetAttrString(pModule, "CreateGrid");
+    if(pClass == NULL)
+    {
+        PyErr_Print();
+        printf("Failed to locate CreateGrid class: please check that you have the latest version of creategrid.py and Python 2.7.x");
+    }
     assert(pClass != NULL);
 
     PyObject* pGridName = PyString_FromString(grid_file_path);
+    if(pGridName == NULL)
+    {
+        PyErr_Print();
+        printf("Failed to create Python string from grid_file_path: please check that grid_file_path is a valid string");
+    }
     assert(pGridName != NULL);
 
     PyObject* pGridNameTuple = PyTuple_Pack(1,pGridName);
+    if(pGridNameTuple == NULL)
+    {
+        PyErr_Print();
+        printf("Failed to create Python tuple: please check that your Python version is 2.7.x");
+    }
     assert(pGridNameTuple != NULL);
 
     PyObject* pInstance = PyInstance_New(pClass, pGridNameTuple, NULL);
+    if(pInstance == NULL)
+    {
+        PyErr_Print();
+        printf("Failed to create instance of CreateGrid: please check that you have the latest version of creategrid.py and Python 2.7.x");
+    }
     assert(pInstance != NULL);
 
     // Cleanup
